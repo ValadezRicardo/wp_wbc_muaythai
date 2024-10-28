508a6f066448237949f5fdc5406c801b36b46d02
RicardoValadez <36349018+ValadezRicardo@users.noreply.github.com>
2024-10-28 12:45:42 -0600

    main commit
    add archives

---
+++ wp-content/mu-plugins/vendor/godaddy/mwc-core/vendor/godaddy/mwc-dashboard/src/API/Controllers/AbstractController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace GoDaddy\WordPress\MWC\Dashboard\API\Controllers;
+
+use GoDaddy\WordPress\MWC\Common\API\Controllers\AbstractController as CommonAbstractController;
+
+/**
+ * Abstract controller class.
+ *
+ * @deprecated use {@see CommonAbstractController} instead.
+ */
+abstract class AbstractController extends CommonAbstractController
+{
+}
